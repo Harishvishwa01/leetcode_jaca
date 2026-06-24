f5a2a5412d9cfea5970b231de37bcab692906d08
vishwa <harishvishwa12003@gmail.com>
2026-06-24 21:07:13 +0530

    added 2 model

---
+++ src/main/java/com/example/leetcode_java/model/Submission.java
@@ -0,0 +1,10 @@
+package com.example.leetcode_java.model;
+
+import java.util.UUID;
+
+public class Submission {
+    private UUID UserId;
+    private  UUID SubmissionId;
+    private  String Lang;
+    private Boolean Status;
+}
---
+++ src/main/java/com/example/leetcode_java/model/User.java
@@ -0,0 +1,9 @@
+package com.example.leetcode_java.model;
+
+import java.util.UUID;
+
+public class User {
+    private String name;
+    private UUID UserId;
+    private int solvedQuestion;
+}
