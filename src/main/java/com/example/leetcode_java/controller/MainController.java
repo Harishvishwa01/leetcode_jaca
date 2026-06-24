0f91d8c1c7046d7cb4946b1c9bcf2e402404edf2
vishwa <harishvishwa12003@gmail.com>
2026-06-24 21:12:46 +0530

    init

--- src/main/java/com/example/leetcode_java/controller/MainController.java
+++ src/main/java/com/example/leetcode_java/controller/MainController.java
@@ -1,15 +1,26 @@
 package com.example.leetcode_java.controller;
 
 
+import com.example.leetcode_java.services.RouteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    @Autowired
+   RouteService routeService;
 
     @GetMapping("/")
     public String greet(){
-        return "hello world";
+        return routeService.greet();
     }
 
+    @PostMapping("/add")
+    public String add(@RequestBody Integer no) {
+        routeService.addToQueue(no);
+        return "added";
+    }
 }
